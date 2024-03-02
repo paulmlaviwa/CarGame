87a5a07a288c2cd44ceb1fdb71f49cf9db4f4b5a
Paul Mlaviwa <mlaviwa.paul@gmail.com>
2024-03-02 22:45:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+user_input = ""
+started = False
+while True:
+    user_input = input("> ").lower()
+    if user_input == "start":
+        if started:
+            print("Car is already started")
+        else:
+            started = True
+            print("Car started... Ready to go!")
+    elif user_input == "stop":
+        if not started:
+            print("Car is already stopped")
+        else:
+            started = False
+            print("Car stopped")
+    elif user_input == "quit":
+        exit()
+    elif user_input == "help":
+        print('''
+start - to start the car
+stop - to stop the car
+quit - to exit
+        ''')
+    else:
+        print(f"Sorry, I don't understand '{user_input}'")
+
